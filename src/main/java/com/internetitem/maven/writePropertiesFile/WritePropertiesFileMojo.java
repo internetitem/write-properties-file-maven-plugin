fdaf2f2cc9450d63624ed0b15f1f15ceadc0118e
Adam Batkin <adam@batkin.net>
2014-02-24 01:32:47 -0500

    add javadoc

--- src/main/java/com/internetitem/maven/writePropertiesFile/WritePropertiesFileMojo.java
+++ src/main/java/com/internetitem/maven/writePropertiesFile/WritePropertiesFileMojo.java
@@ -13,12 +13,24 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+/**
+ * Create a Java Properties file
+ * 
+ * @author Adam Batkin <adam@batkin.net>
+ *
+ */
 @Mojo(name = "write-properties-file")
 public class WritePropertiesFileMojo extends AbstractMojo {
 
+	/**
+	 * File where the properties are saved
+	 */
 	@Parameter(property = "outputFile", required = true)
 	private File outputFile;
 
+	/**
+	 * Properties to save
+	 */
 	@Parameter(property = "properties", required = true)
 	private Properties properties;
 
